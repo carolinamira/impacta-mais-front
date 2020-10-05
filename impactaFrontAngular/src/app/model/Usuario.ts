ac92fe0b9039f0322e038452f7e64e786a9c6934
AndrehW27 <andrehwillian@hotmail.com>
2020-10-05 16:10:58 -0300

    ajuste cadastro

--- impactaFrontAngular/src/app/model/Usuario.ts
+++ impactaFrontAngular/src/app/model/Usuario.ts
@@ -3,7 +3,7 @@ import { Postagem } from './Postagem';
 export class Usuario {
     public id: number;
     public nome: string;
-    public usuario: string;
+    public email: string;
     public senha: string;
     public postagem: Postagem[];
 }
